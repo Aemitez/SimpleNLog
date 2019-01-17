72cb83f1444c18556f00c477abb84fa09f93dd36
Aemitez <watcharapong_aem@hotmail.com>
2019-01-17 14:58:49 +0700

    Add files via upload

---
+++ SimpleNLog/LogHelper.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+namespace SimpleNLog
+{
+    public static class LogHelper
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void WriteLog(LogLevel logLevel, string message)
+        {
+            logger.Log(logLevel, message);
+        }
+    }
+}
---
+++ SimpleNLog/Program.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using System;
+
+namespace SimpleNLog
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            LogHelper.WriteLog(LogLevel.Trace, "Trace Aemitez");
+            LogHelper.WriteLog(LogLevel.Debug, "Debug Aemitez");
+            LogHelper.WriteLog(LogLevel.Info, "Info Aemitez");
+            LogHelper.WriteLog(LogLevel.Warn, "Warn Aemitez");
+            LogHelper.WriteLog(LogLevel.Error, "Error Aemitez");
+            LogHelper.WriteLog(LogLevel.Fatal, "Fatal Aemitez");
+            LogHelper.WriteLog(LogLevel.Off, "Off Aemitez");
+            Console.ReadLine();
+        }
+    }
+}
